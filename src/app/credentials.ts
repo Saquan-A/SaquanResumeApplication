3959c6cd429b4fccb6523b5c3d36f8f66efeed7c
Saquan <saquan12@gmail.com>
2018-08-27 13:23:28 -0400

    First edit

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDPPkkJfice6qChJnaS3ojOCa2JiwUl8pQ',
+  authDomain: 'saquanresume.firebaseapp.com',
+  databaseURL: 'https://saquanresume.firebaseio.com',
+  projectId: 'saquanresume',
+  storageBucket: 'saquanresume.appspot.com',
+  messagingSenderId: '4011148305'
+}
